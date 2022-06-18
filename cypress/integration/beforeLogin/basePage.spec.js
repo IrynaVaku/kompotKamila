67d3596472a7f442ec3117ead6dc9c6d4ac56671
irynaVaku <vakulovskaya@gmail.com>
2022-06-17 21:21:37 -0800

    Created new tests for Pricing

--- cypress/integration/beforeLogin/basePage.spec.js
+++ cypress/integration/beforeLogin/basePage.spec.js
@@ -1,57 +1,20 @@
+import LocalCommands from '../../support/page_object/localCommands';
 import BasePage from '../../support/page_object/beforeLoginPOM/basePage';
-import RegistrationPage from '../../support/page_object/registrationPage';
+import RegistrationPage from '../../support/page_object/beforeLoginPOM/registrationPage';
 const basePage = new BasePage();
 const registrationPage = new RegistrationPage();
+const localCommands = new LocalCommands()
 
 describe('Nav Bar', function () {
   beforeEach('Load base page', () => {
     cy.visit('/').location('pathname').should('eq', '/');
   });
-  it('Logo', function () {
-    basePage
-      .logo()
-      .should('be.visible')
-      .should('have.text', 'Kompot')
-      .click()
-      .location('pathname')
-      .should('eq', '/');
-  });
-  it('Top menu', () => {
-    const topMenu = ['Pricing', 'Industries', 'Support'];
-    topMenu.forEach((el) =>
-      cy
-        .get(`[href="/i/${el.toLowerCase()}"]`)
-        .should('be.visible')
-        .should('have.text', `${el}`)
-        .click()
-        .location('pathname')
-        .should('eq', `/i/${el.toLowerCase()}`)
-        .wait(200)
-        .go('back')
-    );
-  });
-  it('border-button', () => {
-    const borderButton = [
-      ['Log in', 'login'],
-      ['Sign up', 'register'],
-    ];
-    borderButton.forEach((el) =>
-      cy
-        .get(`[data-qa="${el[1]}"]`)
-        .first()
-        .should('have.text', `${el[0]}`)
-        .click()
-        .location('pathname')
-        .should('eq', `/user/${el[1]}`)
-        .wait(200)
-        .go('back')
-    );
-  });
-  it('countTopMenu', () => {
-    cy.get('div#top-menu>div').should('have.length', 3);
-    cy.get('div#top-menu').children().should('have.length', 3);
+  it('Nav Bar menu with local helper', () => {
+localCommands.navigationIntrodactingPage()
+  })
+  
     //each
-    const countTopMenu = [];
+    /*const countTopMenu = [];
     let startCutting;
     let finishCutting;
     let endpoint;
@@ -64,13 +27,7 @@ describe('Nav Bar', function () {
     });
     cy.log(countTopMenu);
   });
-
-  it('countBorderButton', () => {
-    const countBorderButton = [];
-    cy.get('div#top-menu').next().children().should('have.length', 2);
-    cy.get('[data-qa="login"]').siblings().should('have.length', 1);
-    cy.get('[data-qa="login"]').parent().children().should('have.length', 2);
-  });
+*/
   it('countLogo', () => {
     const countLogo = [];
     cy.get('div#top-menu').prev().children().should('have.length', 1);

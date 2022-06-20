e6ec61e49af115f01bc0959c2bf48d784e6faccd
irynaVaku <vakulovskaya@gmail.com>
2022-06-20 06:29:01 -0800

    test pipeline

--- cypress/integration/client/modalClient.spec.js
+++ cypress/integration/client/modalClient.spec.js
@@ -1,18 +1,17 @@
-import ModalClient from "../../support/page_object/modalClient";
-import ClientPage from "../../support/page_object/clientPage";
-const modalClient = new ModalClient()
-const clientPage = new ClientPage()
+import ModalClient from '../../support/page_object/modalClient';
+import ClientPage from '../../support/page_object/clientPage';
+const modalClient = new ModalClient();
+const clientPage = new ClientPage();
 
-describe('Create Client modal', () => {
-    it('modal client page ', function () {
-        cy.modalClientCreate()
-        // modalClient.btnCancel().contains('Cancel').click()
-        // cy.login().wait(1000).visit('/client')
-        // clientPage.btnCreate().contains('Create Client').click()
+describe('Create Client modal first', () => {
+  it('modal client page ', function () {
+    cy.modalClientCreate();
+    // modalClient.btnCancel().contains('Cancel').click()
+    // cy.login().wait(1000).visit('/client')
+    // clientPage.btnCreate().contains('Create Client').click()
 
-        cy.login().wait(1000).visit('/client')
-        clientPage.btnCreate().click().wait(500)
-        modalClient.btnCancel().contains('Cancel').click()
-    })
-    
-})
+    cy.login().wait(1000).visit('/client');
+    clientPage.btnCreate().click().wait(500);
+    modalClient.btnCancel().contains('Cancel').click();
+  });
+});
